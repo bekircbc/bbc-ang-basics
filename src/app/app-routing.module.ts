e66137b8131fe7f70b71bb77a10b233f48891362
Bekir - CDI <b.burakcebecii@gmail.com>
2022-08-29 17:43:46 +0200

    added route for shopping list..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { EmployeesPageComponent } from './employees-page/employees-page.componen
 import { InfoPageComponent } from './info-page/info-page.component';
 import { EmployeeDetailPageComponent } from './employee-detail-page/employee-detail-page.component';
 import { ServersComponent } from './servers/servers.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const routes: Routes = [
   {
@@ -20,6 +21,11 @@ const routes: Routes = [
   {
     path: 'servers',
     component: ServersComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'shoppinglist',
+    component: ShoppingListComponent,
     pathMatch: 'full',
   },
 ];
